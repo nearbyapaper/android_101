b663a02fddc2b3e3ebf1fd68ebf7e2d9272bcc91
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-08-06 15:28:10 +0700

    make AppDialog

--- app/src/main/java/com/example/neardroid/ConfirmActivity.kt
+++ app/src/main/java/com/example/neardroid/ConfirmActivity.kt
@@ -1,18 +1,23 @@
 package com.example.neardroid
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.example.neardroid.util.AppDialog
+import com.example.neardroid.util.OnDialogButtonListener
 
-class ConfirmActivity : AppCompatActivity() {
+class ConfirmActivity : AppCompatActivity(),OnDialogButtonListener {
     // var
     var nameTextView: TextView? = null
     var saberTextView: TextView? = null
     var forceTextView: TextView? = null
     var jediButton: Button? = null
+    var testAppDialogButton: Button ?= null
+    lateinit var dialog: AlertDialog
 
     //end var
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +35,11 @@ class ConfirmActivity : AppCompatActivity() {
             val nextIntent = Intent(this, JediMasterListView::class.java)
             startActivity(nextIntent)
         }
+
+        testAppDialogButton?.setOnClickListener {
+            dialog = AppDialog(this).create(this,"Hello Dialog")
+            dialog.show()
+        }
     }
 
     private fun bindWidget() {
@@ -37,5 +47,16 @@ class ConfirmActivity : AppCompatActivity() {
         saberTextView = findViewById(R.id.saberTextView)
         forceTextView = findViewById(R.id.forceTextView)
         jediButton = findViewById(R.id.jediButton)
+        testAppDialogButton = findViewById(R.id.testAppDialogButton)
+    }
+
+    override fun handlePositiveButton() {
+        Log.d("1995","handlePositiveButton")
+        dialog.cancel()
+    }
+
+    override fun handleNegativeButton() {
+        Log.d("1995","handleNegativeButton")
+        dialog.cancel()
     }
 }
--- app/src/main/java/com/example/neardroid/util/AppDialog.kt
+++ app/src/main/java/com/example/neardroid/util/AppDialog.kt
@@ -3,18 +3,14 @@ package com.example.neardroid.util
 import android.app.AlertDialog
 import android.content.Context
 
-class AppDialog{
-
+class AppDialog(val action: OnDialogButtonListener){
+    fun create(context: Context, message: String): AlertDialog {
+        return AlertDialog.Builder(context).setMessage(message)
+            .setPositiveButton("OK") { dialog, id ->
+                action.handlePositiveButton()
+            }
+            .setNegativeButton("Cancel") { dialog, id ->
+                action.handleNegativeButton()
+            }.create() ?: throw IllegalStateException("Activity cannot be null")
+    }
 }
-
-//class AppDialog(val context: Context, private val action: OnDialogButtonListener) : AlertDialog(context) {
-//    fun create(context: Context, message: String): AlertDialog {
-//        return AlertDialog.Builder(context).setMessage(message)
-//            .setPositiveButton("OK") { dialog, id ->
-//                action.handlePositiveButton()
-//            }
-//            .setNegativeButton("Cancel") { dialog, id ->
-//                action.handleNegativeButton()
-//            }.create() ?: throw IllegalStateException("Activity cannot be null")
-//    }
-//}
